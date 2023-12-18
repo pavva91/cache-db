02c11ab2ca8388207b9938fe047356e6fe2076d5
pavva91 <valerio.mattioli@proton.me>
2023-12-18 12:43:58 +0100

    Base solution works

--- main.go
+++ main.go
@@ -8,43 +8,59 @@ import (
 )
 
 type User struct {
-	ID int
+	ID       int
 	Username string
 }
 
 type Server struct {
-	db map[int]*User
+	db    map[int]*User
+	cache map[int]*User
 	dbhit int
 }
 
 func NewServer() *Server {
 	db := make(map[int]*User)
 	for i := 0; i < 100; i++ {
-		db[i+1]= &User{
-			ID: i + 1,
+		db[i+1] = &User{
+			ID:       i + 1,
 			Username: fmt.Sprintf("user_%d", i+1),
 		}
 	}
 	return &Server{
-		db: db,
+		db:    db,
+		cache: make(map[int]*User),
 	}
 }
 
+func (s *Server) tryCache(id int) (*User, bool) {
+	user, ok := s.cache[id]
+	return user, ok
+}
 
-func (s *Server) handleGetUser(w http.ResponseWriter, r *http.Request)  {
+func (s *Server) handleGetUser(w http.ResponseWriter, r *http.Request) {
 	idStr := r.URL.Query().Get("id")
 	id, _ := strconv.Atoi(idStr)
 
+	// First try to hit the cache
+	user, ok := s.tryCache(id)
+	if ok {
+		json.NewEncoder(w).Encode(user)
+		return
+	}
+
 	// hit the database
-	user, ok := s.db[id]
+	user, ok = s.db[id]
 	if !ok {
 		panic("user not found")
 	}
 	s.dbhit++
 
+	// insert in cache
+	s.cache[id] = user
+
 	json.NewEncoder(w).Encode(user)
 }
 
-func main()  {
-	
+func main() {
+
 }
